b96f44acb59e432ab203d09cc2cf23033828f45a
Ahmed Bilal <ahmed.bilal@wearenova.co.uk>
2024-04-18 19:49:21 +0500

    Login success/failure!

---
+++ CLIKWeb/src/test/java/CLIK/CLIKWeb/CLIKLogin.java
@@ -0,0 +1,109 @@
+package CLIK.CLIKWeb;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class CLIKLogin {
+
+	WebDriver driver;
+
+	@BeforeTest
+	public void beforeTest() 
+
+	{
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://app.s.clik.audio/login");
+	}
+
+	@AfterTest
+	public void afterTest() 
+	{
+		driver.quit();
+	}
+
+
+	@Test(priority=1)
+	public void Login_failure() throws InterruptedException 
+	{
+		Thread.sleep(3000);
+		
+		System.out.println("Login Failure TC initiated...");
+
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/form/div[1]/div/div/div/input")).sendKeys("a@a.com");
+		
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/form/div[2]/div/div/div/input")).sendKeys("sss");
+		
+		
+		Thread.sleep(3000);
+
+//		js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+		JavascriptExecutor js = (JavascriptExecutor) driver; js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+		
+		Thread.sleep(3000);
+
+		
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/form/button[2]/span")).click();
+		
+		Thread.sleep(3000);
+
+		String Failure_msg= driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div/div[1]/div[2]")).getText();
+
+		System.out.println(Failure_msg);
+		
+		Thread.sleep(1000);
+		
+		
+	}
+	
+	@Test(priority=2)
+	public void Login_success() throws InterruptedException 
+	{
+		Thread.sleep(2000);
+		
+		System.out.println("Login Success TC initiated...");
+
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/form/div[1]/div/div/div/input")).click();
+
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/form/div[1]/div/div/div/input")).clear();
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/form/div[1]/div/div/div/input")).sendKeys("ahmed.bilal+admin@fusiontech.global");
+		
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/form/div[2]/div/div/div/input")).click();
+
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/form/div[2]/div/div/div/input")).clear();
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/form/div[2]/div/div/div/input")).sendKeys("Liverpool1");
+		
+		
+		Thread.sleep(3000);
+
+//		js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+//		JavascriptExecutor js = (JavascriptExecutor) driver; js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+//		
+//		Thread.sleep(3000);
+
+		
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/form/button[2]/span")).click();
+		
+		Thread.sleep(3000);
+
+		String Dashboard_title= driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[1]/div[1]/div/span")).getText();
+
+		System.out.println(Dashboard_title);
+		
+		Thread.sleep(1000);
+		
+		
+	}
+
+	
+
+}
